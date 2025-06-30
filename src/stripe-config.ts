00cd5482b10ee1874aa5de27904b7f9b9314a814
Ryan SABOWA <screentapinc@gmail.com>
2025-06-30 02:14:17 +0100

    Updated stripe-config.ts

--- src/stripe-config.ts
+++ src/stripe-config.ts
@@ -11,7 +11,7 @@ export interface Product {
 export const products: Product[] = [
   {
     id: 'prod_SahUAARvVVXQLg',
-    priceId: 'price_1RfUwkAmXOVRZkyi5T6CUqax',
+    priceId: 'price_1RfW5rPPWwWvhRQtcWbx0KuY',
     name: 'Hookline',
     description: 'Pack de 10 phrases d\'accroche personnalisées générées par IA',
     mode: 'payment',
